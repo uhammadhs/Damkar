82eb08019515d28c666d1da36675f94eed7a4e84
Muhammad Najib <nm6201160@gmail.com>
2025-07-17 16:14:41 +0000

    NEXT_PUBLIC_SUPABASE_URL=https://vhkbolywudzcpnjvzncv.supabase.co NEXT_P

--- src/lib/supabase/config.ts
+++ src/lib/supabase/config.ts
@@ -1,4 +1,4 @@
 // /src/lib/supabase/config.ts
-export const supabaseUrl = "https://valwjihmebezujxihqdl.supabase.co";
+export const supabaseUrl = "https://vhkbolywudzcpnjvzncv.supabase.co";
 
-export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhbHdqaWhtZWJlenVqeGlocWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MDI3MTIsImV4cCI6MjA2ODI3ODcxMn0.fQ7Vxnwfix8HsERcbHnS8RsJQKv-9hdo5eM1TVjtK1M";
+export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoa2JvbHl3dWR6Y3BuanZ6bmN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3NjgxMjQsImV4cCI6MjA2ODM0NDEyNH0.nzmSuN_2ghIIgLif-jvEkHVACi70k7bpI8Xy6xGywvU";
